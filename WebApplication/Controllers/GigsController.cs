4282897b888aec024b730b4ea66bf3bf97f5935c
Mohammed Fauz Usman 11715031 <mohammed.11715031@lpu.in>
2020-07-28 23:26:59 +0530

    Implement saving a gig.

--- WebApplication/Controllers/GigsController.cs
+++ WebApplication/Controllers/GigsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
 using System.Web.Mvc;
 using WebApplication.Models;
 using WebApplication.ViewModels;
@@ -13,7 +15,7 @@ namespace WebApplication.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        
+        [Authorize]
         public ActionResult Create()
         {
 
@@ -23,6 +25,23 @@ namespace WebApplication.Controllers
             };
            
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
